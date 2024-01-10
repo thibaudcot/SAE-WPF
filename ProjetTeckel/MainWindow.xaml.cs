a0db19fb56743b519ac50a0cddf2b73c1a861345
Thibaud-Thai Cottarel <Thibaud-Thai.Cottarel@etu.univ-smb.fr>
2024-01-10 11:49:10 +0100

    bouton pour ce deplacer marche

--- ProjetTeckel/MainWindow.xaml.cs
+++ ProjetTeckel/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
@@ -30,22 +31,45 @@ namespace ProjetTeckel
             InitializeComponent();
         }
 
-        private void teckel_ToucheBas(object envoye, KeyEventArgs e)
+        private void PrjTeckel_KeyDown(object sender, KeyEventArgs e)
         {
             int ligneRec = Grid.GetRow(teckel);
             int colRec = Grid.GetColumn(teckel);
             switch (e.Key.ToString())
             {
-                case "Haut":
+                case "Up":
                     if (ligneRec > 0)
                     {
                         ligneRec = ligneRec - 1;
                         Grid.SetRow(teckel, ligneRec);//déplace le rectangle sur ces nouvelles coordonnées
                     }
                     break;
+
+                case "Down":
+                    if (ligneRec < 15)
+                    {
+                        ligneRec = ligneRec + 1;
+                        Grid.SetRow(teckel, ligneRec);
+                    }
+                    break;
+
+                case "Right":
+                    // Check if collided
+                    if (colRec < 9)
+                    {
+                        colRec = colRec + 1;
+                        Grid.SetColumn(teckel, colRec);
+                    }
+                    break;
+                case "Left":
+                    // Check if collided
+                    if (colRec > 0)
+                    {
+                        colRec = colRec - 1;
+                        Grid.SetColumn(teckel, colRec);
+                    }
+                    break;
             }
         }
-
-       
     }
 }
