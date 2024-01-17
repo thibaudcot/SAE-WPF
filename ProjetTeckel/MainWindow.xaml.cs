c0ee9540965d26778cddb8dd2f4067dd11303f67
Anes Kettaf <anes.kettaf@etu.univ-smb.fr>
2024-01-17 08:45:17 +0100

    score maj + chocolat maj

--- ProjetTeckel/MainWindow.xaml.cs
+++ ProjetTeckel/MainWindow.xaml.cs
@@ -113,6 +113,13 @@ namespace ProjetTeckel
                     int xChocolat = randomC.Next(0, 17);
                     int yChocolat = randomC.Next(0, 20);
 
+                    // Générer des coordonnées aléatoires pour le chocolat en évitant l'emplacement de l'os
+                    do
+                    {
+                        xChocolat = randomC.Next(0, 17);
+                        yChocolat = randomC.Next(0, 20);
+                    } while (xChocolat == Grid.GetRow(nourriture) && yChocolat == Grid.GetColumn(nourriture));
+
                     chocolat.Width = teckel.Width;
                     chocolat.Height = teckel.Height;
 
@@ -130,8 +137,9 @@ namespace ProjetTeckel
                 if (Grid.GetRow(teckel) == Grid.GetRow(nourriture) && Grid.GetColumn(teckel) == Grid.GetColumn(nourriture))
                 {
                     Grid.Children.Remove(nourriture);
-                    score++;
+                    score ++;
                     Score();
+                    AjouterPartieCorpsChien();
                 }
 
                 foreach (ChocolatInfo chocolatInfo in listeChocolats.ToList())
@@ -291,7 +299,7 @@ namespace ProjetTeckel
             if (score > 0)
             {
                 RetirerPartieCorpsChien();
-                score--; // Diminuer le score lorsqu'un chocolat est mangé
+ 
                 Score();
             }
         }
